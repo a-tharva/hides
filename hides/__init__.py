fb71dde2f5968693c73fed709d4243099426dc20
atharva bhandvalkar <bhandvalkaratharva@gmail.com>
2023-08-15 17:13:46 +0530

    added setup and init changes

--- hides/__init__.py
+++ hides/__init__.py
@@ -1,2 +1,8 @@
+__author__="Atharva Bhandvalkar"
+__email__="<atharv.bhandvalkar@gmail.com>"
+
+__license__="MIT"
+__name__="HIDES"
 __version__="0.0.1"
-__title__=="HIDES"
+__title__="HIDES"
+__description__="Tool to scan encript and sign files"
--- hides/hides.py
+++ hides/hides.py
@@ -2,18 +2,19 @@ import argparse
 
 
 def main() -> None:
-    
+
     parser = argparse.ArgumentParser(description='Run HIDES')
-    parser.add_argument('-f', '--file', type=str, help="input file location", nargs="?")
-    parser.add_argument('-i', '--inspect', type=str, help="Scan/inspect file for virus or vulnerability", metavar='')
+    parser.add_argument('-f', '--file', type=str,
+                        help="input file location", nargs="?")
+    parser.add_argument('-i', '--inspect', type=str,
+                        help="Scan/inspect file for virus or vulnerability", metavar='')
     parser.add_argument('-v', '--version', help="Version", action="store_true")
 
     args = parser.parse_args()
-    
+
     if args.version:
         print(f"{__title__} version {__version__}")
         return
-    
 
 
 if __name__ == '__main__':
